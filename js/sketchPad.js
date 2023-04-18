0bcb5734c39a22bd1ac0f5d268005af493848125
ahmadbarzak <ahmadmbarzak@gmail.com>
2023-04-18 16:17:25 +1200

    Updated mouse release to work for whole document

--- js/sketchPad.js
+++ js/sketchPad.js
@@ -55,7 +55,7 @@ class SketchPad{
                 this.#redraw();
             }
         }
-        this.canvas.onmouseup=()=>{
+        document.onmouseup=()=>{
             this.isDrawing=false;
         }
         this.refresh.onclick=()=>{
@@ -81,8 +81,8 @@ class SketchPad{
             const loc = evt.touches[0];
             this.canvas.onmousemove(loc);
         }
-        this.canvas.ontouchend=()=>{
-            this.canvas.onmouseup();
+        document.ontouchend=()=>{
+            document.onmouseup();
         }
     }
 
